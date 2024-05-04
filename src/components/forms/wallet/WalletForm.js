3236ad0a056b4f671e5f14b9bf3bc40145039727
wendel filipe <wendelfilipe1997@hotmail.com>
2024-05-04 15:12:42 -0300

    building frontend: create getAssets

--- src/components/forms/wallet/WalletForm.js
+++ src/components/forms/wallet/WalletForm.js
@@ -1,14 +1,57 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
+import api from "../../../api/Api";
+import AssetsHome from "../../assets/AssetsHome";
 
 const WalletForm = (props) => {
-    const name = "Wallet";
-    const  [ process, setProcess ]  = useState(25);
+    let [ process, setProcess ]  = useState(0);
+    let [ assetsData, setAssetsData ] = useState([]);
+    const [totalAssets, setTotalAssets ] = useState(0)
+    debugger
+    let [ perCent, setPerCent ] = useState(0);
 
-    function walletIdCookie(){
+    const cookies = document.cookie.split(';').reduce((cookies, cookie) => {
+        const [name, value] = cookie.split('=').map(cookie => cookie.trim());
+        cookies[name] = value;
+        return cookies;
+    }, {});
 
-    
+    const walletIdString = cookies.WalletIdCookie;
+    const walletId = parseInt(walletIdString, 10);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            await getAssets();
+
+            await sumAllAssets();
+        };
+
+        fetchData();
+    }, []);
+
+
+
+    async function getAssets(){
+        
+        const assets = await api.get(`assets/GetAllAssetsDTOAsync/${walletId}`)
+        const assetsData = assets.data;
+        setAssetsData(assetsData)
+        debugger
     }
+
+    async function sumAllAssets(){
+        await Promise.all(
+            assetsData.map(a => {
+                debugger
+                let totalEachAssets = a.amount * a.buyPrice;
+                totalAssets += totalEachAssets;
+            })
+        );
+        setTotalAssets(totalAssets);
+        setPerCent(totalAssets)
+        debugger
+    }
+
 
 
     return(
@@ -19,15 +62,15 @@ const WalletForm = (props) => {
                     <div className="card-body">
                     <h5 className="card-title"></h5>
                     <p className="card-text"></p>
-                    <a className="text-decoration-none" href="/createassets" onClick={walletIdCookie}>
+                    <a className="text-decoration-none" href="/assetshome">
                         <i className="fa-solid fa-wallet"></i>
-                        <p className="card-text ms-2 ts-sm d-inline-block me-1">{name}</p>
+                        <p className="card-text ms-2 ts-sm d-inline-block me-1">{props.nameAssets}</p>
                     </a>
                     </div>
                     <div className="progress-stacked">
-                        <div className="progress" role="progressbar" aria-label="Segment one" aria-valuenow="15" aria-valuemin="0" aria-valuemax="100" style={{width: `${process}%`}}>
+                        <div className="progress" role="progressbar" aria-label="Segment one" aria-valuenow="15" aria-valuemin="0" aria-valuemax="100" style={{width: `${perCent}%`}}>
                             <div className="progress-bar">
-                                {process + "%"}
+                                {perCent + "%"}
                             </div>
                         </div>
                     </div>
